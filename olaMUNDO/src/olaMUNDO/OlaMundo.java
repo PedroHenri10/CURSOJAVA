a66ddc9f6dcd357ebc58184b362a4a4d254eeb99
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-02-04 22:58:05 -0300

    PRIMEIROS PASSOS

---
+++ olaMUNDO/src/olaMUNDO/OlaMundo.java
@@ -0,0 +1,30 @@
+package olaMUNDO;
+import java.util.Scanner;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ola Mundo!");
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int idade;
+		
+		System.out.println("Qual a sua idade");
+		
+		idade = leia.nextInt();
+		
+		System.out.println("Minha idade é "+ idade);
+		
+		Scanner input = new Scanner(System.in);
+		
+		String nome;
+		
+		System.out.println("Qual O SEU NOME");
+		
+		nome = input.nextLine();
+		
+	}
+
+}
---
+++ sistemaSoma/src/sistemaSoma/SistemaSoma.java
@@ -0,0 +1,23 @@
+package sistemaSoma;
+import java.util.Scanner;
+
+public class SistemaSoma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+		int valor1, valor2;
+		
+		System.out.println("Insira o primeiro valor: ");
+		valor1 = input.nextInt();
+		
+		System.out.println("Insira o segundo valor: ");
+		valor2 = input.nextInt();
+		
+		int resultado = valor1 + valor2;
+		
+		System.out.println("O resultado da soma de "+ valor1 + " + "+valor2+" é igual a " +resultado);	
+	}
+
+}
