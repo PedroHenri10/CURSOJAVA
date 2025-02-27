15f465865d6b36a1561f2533fb22493608f0edc7
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-02-26 22:57:23 -0300

    add, remover, listar funcionando

--- projetoToDoList/src/projetoToDoList/Main.java
+++ projetoToDoList/src/projetoToDoList/Main.java
@@ -14,6 +14,7 @@ public class Main {
 		while(op != 0) {
 			System.out.println("1 - Adicionar tarefa \n"+"2 - Remover \n"+"3 - Listar Tarefas \n"+"0 - Sair");
 			op = input.nextInt();
+			input.nextLine();
 			switch(op) {
 			case 0:
 				System.out.println("Saindo...");
@@ -22,6 +23,7 @@ public class Main {
 				adicionarTarefa(listaDeTarefas, input);
 				break;
 			case 2:
+				removerTarefas(listaDeTarefas, input);
 				break;
 			case 3:
 				listarTarefas(listaDeTarefas, input);
@@ -30,6 +32,7 @@ public class Main {
 				System.out.println("Opção Invalida!\n");
 			}
 		}
+		input.close();
 	}
 	private static void adicionarTarefa(ArrayList<Tarefa> listaDeTarefas, Scanner input){
 		Tarefa tarefa = new Tarefa();
@@ -38,9 +41,7 @@ public class Main {
         System.out.print("Digite se o status da tarefa está concluído ou em andamento: ");
         tarefa.setStatus(input.nextLine());
         listaDeTarefas.add(tarefa);
-        System.out.print("Tarefa adicionada!");
-		listaDeTarefas.add(tarefa);
-		
+        System.out.print("Tarefa adicionada!");		
 		//for (Tarefa : tarefas) {
 			//if()
 		}
@@ -53,6 +54,19 @@ public class Main {
 		for(Tarefa t : listaDeTarefas) {
 			System.out.println(t.getTarefa()+"- "+t.getStatus());
 		}
+	}
+	private static void removerTarefas(ArrayList<Tarefa> listaDeTarefas, Scanner input) {
+		System.out.println("digite a tarefa que deseja remover: ");
+		String tarefaRemover = input.nextLine();
+		
+		for(Tarefa t : listaDeTarefas) {
+			if( tarefaRemover.equals(t.getTarefa())) {
+				listaDeTarefas.remove(t);
+				 System.out.println("Tarefa removida!");
+	              return;
+			}
+		}
+        System.out.println("Tarefa não encontrada.");
 	}
 }
 
