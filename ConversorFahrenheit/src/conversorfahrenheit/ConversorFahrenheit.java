4560138b5f1e4cac4980769ef4fbf24950de3a6e
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-09-02 19:46:31 -0300

    desafio aula LP

---
+++ ConversorFahrenheit/src/conversorfahrenheit/ConversorFahrenheit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conversorfahrenheit;
+
+/**
+ *
+ * @author 2830482411031
+ */
+public class ConversorFahrenheit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
