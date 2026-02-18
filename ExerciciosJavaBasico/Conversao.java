502b3cfda4ec0f665c45e920a2a6a583b545f457
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2026-02-17 23:13:59 -0300

    Create Conversao.java

---
+++ ExerciciosJavaBasico/Conversao.java
@@ -0,0 +1,14 @@
+class Conversao{
+  public static void main(String[] args){
+    float valor=11.25f;
+    int conversao = (int) valor;
+
+    System.out.println(valor);
+    System.out.println(cpnversao);
+
+    int quantidade = 200;
+    long qtdLong = quantidade;  
+  System.out.println(qtdLong);
+
+  }
+}
