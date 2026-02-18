929a8157796515058a7cef3d4f1187da113ae143
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2026-02-17 22:45:17 -0300

    Create DescontoE.java

---
+++ ExerciciosJavaBasico/DescontoE.java
@@ -0,0 +1,34 @@
+
+class DescontoE {
+    public static void main(String[] args) {
+        float valor = 200.0f;
+        valor -=20;
+        valor -=30;
+        System.out.printf("valor final descontado: %.2f", valor);
+    }
+}
+
+class Invest{
+  public static void main(String[] args){
+    float valor = 1000;
+    valor *=2;
+    valor *=1.5;
+    System.out.printf("valor final investido: %.2f", valor);
+  }
+}
+
+class Porcao{
+  public static void main(String[] args)[
+    float porcao =400;
+    porcao=porcao/4;
+    System.out.printf("%.2f grama por pessoa", porcao);
+  }
+}
+
+class troco{
+  public static void main(String[] args)[
+    int valor = 17;
+    valor = valor/4;
+    System.out.printf("%.2f ", valor);
+  }    
+}
