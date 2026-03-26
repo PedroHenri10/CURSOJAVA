97ccd6a8c3b4807a97a76acf44dc8c91c7c7ae87
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2026-03-26 19:37:03 -0300

    Create ProdutoHash.java

---
+++ ExerciciosJavaBasico/ProdutoHash.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class ProdutoHash{
+
+    String nome;
+    int codigo;
+
+    public Produto(String nome, int codigo) {
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto)) return false;
+
+        Produto p = (Produto) o;
+        return codigo == p.codigo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+
+    @Override
+    public String toString() {
+        return nome + " (Código: " + codigo + ")";
+    }
+}
+
+public class TesteProduto {
+    public static void main(String[] args) {
+
+        HashSet<Produto> set = new HashSet<>();
+
+        set.add(new Produto("Mouse", 1));
+        set.add(new Produto("Teclado", 2));
+        set.add(new Produto("Mouse", 1)); // duplicado
+
+        System.out.println(set);
+    }
+}
