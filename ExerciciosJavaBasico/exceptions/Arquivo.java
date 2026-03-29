6604bc0b0f792f85bb9da2ca3b68d7220c7166a6
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2026-03-28 21:31:08 -0300

    Create Arquivo.java

---
+++ ExerciciosJavaBasico/exceptions/Arquivo.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Arquivo {
+
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("arquivo.txt"));
+
+            String linha;
+
+            while ((linha = br.readLine()) != null) {
+                System.out.println(linha);
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println("Erro ao ler arquivo!");
+        }
+    }
+}
