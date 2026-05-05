583b9b1e3fcefd16f8382f5e53f891c6dab40bb6
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2026-05-04 23:06:10 -0300

    Update Main.java

--- ExerciciosJavaBasico/Reduce/Main.java
+++ ExerciciosJavaBasico/Reduce/Main.java
@@ -13,6 +13,19 @@ public class Main {
                 .reduce(0, Integer::sum);
         System.out.println(soma2);
 
-        
+        List<Integer> lista3 = Arrays.asList(5, 12, 20, 8, 30);
+        int count = lista3.stream()
+                .filter(n -> n > 10)
+                .reduce(0, (acc, n) -> acc + 1);
+        System.out.println(count);
+
+        List<Integer> lista4 = Arrays.asList(1,2,3,4,5);
+        int soma4 = lista4.stream().reduce(0, Integer::sum);
+        System.out.println(soma4);
+
+        List<Integer> lista5 = Arrays.asList(1, 2, 3, 4);
+        int produto = lista5.stream()
+                .reduce(1, (a, b) -> a * b);
+        System.out.println(produto);
     }
 }
